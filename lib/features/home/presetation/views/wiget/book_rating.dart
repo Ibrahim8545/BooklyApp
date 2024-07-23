09e7c9b29082cbb2acf00ee80730e88d997ce117
ibrahim8545 <ibrahimmostafa122020@gmail.com>
2024-07-23 21:21:01 +0300

    add rating to book details

--- lib/features/home/presetation/views/wiget/book_rating.dart
+++ lib/features/home/presetation/views/wiget/book_rating.dart
@@ -1,29 +1,34 @@
-
 import 'package:flutter/material.dart';
 
 import '../../../../../core/utils/systles.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
-
+  BookRating({super.key, this.mainAxisAlignment = MainAxisAlignment.start});
+  final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
-    return  Row(
+    return Row(
+      mainAxisAlignment: mainAxisAlignment,
       children: [
         const Icon(
           Icons.star,
+          size: 20,
           color: Color(0xffFFDD4F),
         ),
-        const  SizedBox(width: 6.3,),
+        const SizedBox(
+          width: 6.3,
+        ),
         const Text(
           '4.8',
-          style: Styles.textStyle14,
+          style: Styles.textStyle16,
         ),
-
-        const  SizedBox(width: 5,),
-        Text(
-          '(2330)',
-          style: Styles.textStyle14.copyWith(color: Color(0xff707070)),
+        const SizedBox(
+          width: 5,
+        ),
+        Opacity(
+          opacity: .5,
+          child: Text('(2330)',
+              style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w600)),
         ),
       ],
     );
--- lib/features/home/presetation/views/wiget/book_view_body.dart
+++ lib/features/home/presetation/views/wiget/book_view_body.dart
@@ -1,25 +1,52 @@
+import 'package:booklyapp/core/utils/systles.dart';
+import 'package:booklyapp/features/home/presetation/views/wiget/book_rating.dart';
 import 'package:booklyapp/features/home/presetation/views/wiget/custom_book_details_app_bar.dart';
 import 'package:booklyapp/features/home/presetation/views/wiget/custom_list_view_items.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/systles.dart';
+
+
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var width=MediaQuery.of(context).size.width;
-    return  Padding(
-
-      padding:const   EdgeInsets.symmetric(horizontal:30),
-      child:  Column(
+    var width = MediaQuery.of(context).size.width;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
         children: [
           const CustomBookDetailsAppBar(),
+         
+          
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width *.17),
-            child:const CustomBookImage(),
-          )
-          ],
+            padding: EdgeInsets.symmetric(horizontal: width * .17),
+            child: const CustomBookImage(),
+          ),
+          const SizedBox(height: 47,),
+          Text(
+            'The Jungle Book',
+            style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 6,),
+           Text(
+            'Rudyard Kilpling',
+            style: Styles.textStyle18.copyWith(
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.w500,
+              color: Colors.white.withOpacity(.7)
+            ),
+          ),
+          const SizedBox(height: 18,),
+          BookRating(
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+        ],
       ),
     );
   }
 }
+
+
