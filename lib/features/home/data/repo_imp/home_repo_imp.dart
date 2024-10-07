00ab47a2aefe813c00242b9910c8a13434f9c056
ibrahim8545 <ibrahimmostafa122020@gmail.com>
2024-10-07 20:05:01 +0300

    add home repo imp as featered books

--- lib/features/home/data/repo_imp/home_repo_imp.dart
+++ lib/features/home/data/repo_imp/home_repo_imp.dart
@@ -1,14 +1,31 @@
 import 'package:booklyapp/core/errors/failure.dart';
+import 'package:booklyapp/features/home/data/data_sources/home_local_data_sources.dart';
 import 'package:booklyapp/features/home/domain/enties/book_entity.dart';
 import 'package:booklyapp/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepoImp extends HomeRepo
 {
+  final HomeRemoteDataSourcse homeRemoteDataSourcse;
+  final HomeLocalDataSourcseImp homeLocalDataSourcse;
+
+  HomeRepoImp({required this.homeLocalDataSourcse,required this.homeRemoteDataSourcse});
   @override
-  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks()async
+   {
+    
+    try {
+      var booklist=homeLocalDataSourcse.fetchFeaturedBooks();
+      if(booklist.isNotEmpty)
+      {
+        return right(booklist);
+      }
+      var book=await homeRemoteDataSourcse.fetchFeaturedBooks();
+      return right(book);
+    } catch (e) {
+      return left(Failure());
+    }
+  
   }
 
   @override
