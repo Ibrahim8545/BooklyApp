4674d62dcc66d45fa34984a78e711a8692b7213d
ibrahim8545 <ibrahimmostafa122020@gmail.com>
2024-10-07 21:08:18 +0300

    custmize failre class

--- lib/features/home/data/repo_imp/home_repo_imp.dart
+++ lib/features/home/data/repo_imp/home_repo_imp.dart
@@ -3,6 +3,7 @@ import 'package:booklyapp/features/home/data/data_sources/home_local_data_source
 import 'package:booklyapp/features/home/domain/enties/book_entity.dart';
 import 'package:booklyapp/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImp extends HomeRepo
 {
@@ -13,17 +14,22 @@ class HomeRepoImp extends HomeRepo
   @override
   Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks()async
    {
-    
+     List<BookEntity> books;
     try {
-      var booklist=homeLocalDataSourcse.fetchFeaturedBooks();
-      if(booklist.isNotEmpty)
+     books=homeLocalDataSourcse.fetchFeaturedBooks();
+      if(books.isNotEmpty)
       {
-        return right(booklist);
+        return right(books);
       }
-      var book=await homeRemoteDataSourcse.fetchFeaturedBooks();
-      return right(book);
+      books=await homeRemoteDataSourcse.fetchFeaturedBooks();
+      return right(books);
     } catch (e) {
-      return left(Failure());
+      if(e is DioError)
+      {
+             return left(ServerFailure.fromDiorError(e));
+
+      }
+ 
     }
   
   }
@@ -31,15 +37,20 @@ class HomeRepoImp extends HomeRepo
   @override
   Future<Either<Failure, List<BookEntity>>> fetchNewsBooks() async{
        try {
-      var booklist=homeLocalDataSourcse.fetchNewsBooks();
-      if(booklist.isNotEmpty)
+       List<BookEntity> books;
+       books=homeLocalDataSourcse.fetchNewsBooks();
+      if(books.isNotEmpty)
       {
-        return right(booklist);
+        return right(books);
       }
-      var book=await homeRemoteDataSourcse.fetchNewsBooks();
-      return right(book);
+      books=await homeRemoteDataSourcse.fetchNewsBooks();
+      return right(books);
     } catch (e) {
-      return left(Failure());
+       if(e is DioError)
+      {
+             return left(ServerFailure.fromDiorError(e));
+
+      }
     }
   }
 
