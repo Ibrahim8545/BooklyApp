4a0b23c0f795e1b4f194e71c2e848019d6b80204
ibrahim8545 <ibrahimmostafa122020@gmail.com>
2024-10-12 18:29:46 +0300

    imp featured books

--- lib/features/home/presetation/manger/reatured_book_cubit/featured_book_cubit.dart
+++ lib/features/home/presetation/manger/reatured_book_cubit/featured_book_cubit.dart
@@ -1,8 +1,27 @@
+import 'package:booklyapp/features/home/domain/use_cases/fetch_featured_books_use_case.dart';
 import 'package:booklyapp/features/home/presetation/manger/reatured_book_cubit/featured_book_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState>
 {
-  FeaturedBooksCubit():super(FeaturedBooksIntitial());
+  FeaturedBooksCubit(this.fetchFeaturedBooksUseCase):super(FeaturedBooksIntitial());
+  final FetchFeaturedBooksUseCase fetchFeaturedBooksUseCase;
+
+  Future<void>fetchBooks()async
+  {
+
+    emit(FeaturedBooksLoading());
+    var result=await fetchFeaturedBooksUseCase.call();
+    result.fold(
+      (failure) {
+        emit(FeaturedBooksError(failure.message));
+      },
+      (books) {
+        emit(FeaturedBooksSuccess(books));
+      },
+    );
+      
+    
+  }
   
 }
